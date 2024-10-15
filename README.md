1f18bab7804ce224985c54bf7de6556908a0ba51
sakthilkv <sakthi13072005@gmail.com>
2024-10-15 13:51:33 +0530

    Added: Rook and Bishop moves

--- README.md
+++
@@ -1,5 +0,0 @@
-#Eevee is an chess engine that is made using python
-
-It uses 2D char array to maintain the board representation.
-
-Work in progress...
---
+++ core/move.py
@@ -0,0 +1,140 @@
+
+from board import load_fen
+
+
+def rook_moves(pos,board):
+
+    moves = []
+    color = board[pos[0]][pos[1]]
+
+    #upward
+    i,j = pos
+    i -= 1
+    while i >= 0:
+        
+        if board[i][j] == "":
+            moves.append((i,j))
+            i -= 1
+        elif board[i][j].isupper() == color.isupper():
+            break
+        else:
+            moves.append((i,j))
+            break
+
+    #downward
+    i,j = pos
+    i += 1
+    while i < 8:
+        
+        if board[i][j] == "":
+            moves.append((i,j))
+            i += 1
+        elif board[i][j].isupper() == color.isupper():
+            break
+        else:
+            moves.append((i,j))
+            break
+    #left
+    i,j = pos
+    j -= 1
+    while j >= 0:
+        
+        if board[i][j] == "":
+            moves.append((i,j))
+            j -= 1
+        elif board[i][j].isupper() == color.isupper():
+            break
+        else:
+            moves.append((i,j))
+            break
+    #right
+    i,j = pos
+    j += 1
+    while j < 8:
+        
+        if board[i][j] == "":
+            moves.append((i,j))
+            j += 1
+        elif board[i][j].isupper() == color.isupper():
+            break
+        else:
+            moves.append((i,j))
+            break
+
+    return moves
+
+def bishop_moves(pos, board):
+
+    moves = []
+    color = board[pos[0]][pos[1]]
+
+    #north-west
+    i,j = pos
+    i -= 1
+    j -= 1
+    while i >= 0 and j >=0:
+        
+        if board[i][j] == "":
+            moves.append((i,j))
+            i -= 1
+            j -= 1
+        elif board[i][j].isupper() == color.isupper():
+            break
+        else:
+            moves.append((i,j))
+            break
+       
+
+    #south-west
+    i,j = pos
+    i += 1
+    j -= 1
+    while i < 8 and j >= 0:
+        
+        if board[i][j] == "":
+            moves.append((i,j))
+            i += 1
+            j -= 1
+        elif board[i][j].isupper() == color.isupper():
+            break
+        else:
+            moves.append((i,j))
+            break
+
+    #north-east
+    i,j = pos
+    i -= 1
+    j += 1
+    while i >= 0 and j < 8:
+        
+        if board[i][j] == "":
+            moves.append((i,j))
+            i -= 1
+            j += 1
+        elif board[i][j].isupper() == color.isupper():
+            break
+        else:
+            moves.append((i,j))
+            break
+    
+    #south-east
+    i,j = pos
+    i += 1
+    j += 1
+    while i < 8 and  j < 8:
+        
+        if board[i][j] == "":
+            moves.append((i,j))
+            i += 1
+            j += 1
+        elif board[i][j].isupper() == color.isupper():
+            break
+        else:
+            moves.append((i,j))
+            break
+    
+    return moves
+
+
+board = load_fen("initial")
+print(rook_moves((4,1),board))
