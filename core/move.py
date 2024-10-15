577e6e9cec04bca7305ea8f40d27581a75b64f53
sakthilkv <sakthi13072005@gmail.com>
2024-10-15 17:09:44 +0530

    Added: Knight Moves

--- core/move.py
+++ core/move.py
@@ -1,6 +1,6 @@
 
 from board import load_fen, debug
-
+from decorators.bench import timeit
 
 def rook_moves(pos,board):
 
@@ -135,9 +135,52 @@ def bishop_moves(pos, board):
     
     return moves
 
+
 def queen_moves(pos,board):
     moves = [*rook_moves(pos,board), *bishop_moves(pos,board)]
     return moves
 
-board = load_fen("initial")
+def knight_moves(pos,board):
+    
+    moves = []
+    color = board[pos[0]][pos[1]]
+
+    i,j = pos
+
+    if i-1 >= 0 and j-2 >= 0:
+        if board[i-1][j-2] == "" or board[i-1][j-2].isupper() != color.isupper():
+            moves.append((i-1,j-2))
+    
+    if i-2 >= 0 and j-1 >= 0:
+        if board[i-2][j-1] == "" or board[i-2][j-1].isupper() != color.isupper():
+            moves.append((i-2,j-1))
+    
+    if i+1 < 8 and j-2 >=0:
+        if board[i+1][j-2] == "" or board[i+1][j-2].isupper() != color.isupper():
+            moves.append((i+1,j-2))
+
+    if i+2 < 8 and j-1 >= 0:
+        if board[i+2][j-1] == "" or board[i+2][j-1].isupper() != color.isupper():
+            moves.append((i+2,j-1))
+
+    if i+2 < 8 and j+1 < 8:
+        if board[i+2][j+1] == "" or board[i+2][j+1].isupper() != color.isupper():
+            moves.append((i+2,j+1))
+    
+    if i+1 < 8 and j+2 < 8:
+        if board[i+1][j+2] == "" or board[i+1][j+2].isupper() != color.isupper():
+            moves.append((i+1,j+2))
+    
+    if i-2 >= 0 and j+1 < 8:
+        if board[i-2][j+1] == "" or board[i-2][j+1].isupper() != color.isupper():
+            moves.append((i-2,j+1))
+
+    if i-1 >= 0 and j+2 < 8:
+        if board[i-1][j+2] == "" or board[i-1][j+2].isupper() != color.isupper():
+            moves.append((i-1,j+2))
+    
+    return moves
+
+board = load_fen("a")
+print((knight_moves((0,0),board)))
 debug(queen_moves((4,3),board))
